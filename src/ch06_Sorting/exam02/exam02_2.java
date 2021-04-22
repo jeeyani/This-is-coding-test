b3df2740ff0af3b6d9562571077c449fb59bd656
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-04-22 22:31:45 +0900

    기초 정렬문제 2번

---
+++ src/ch06_Sorting/exam02/exam02_2.java
@@ -0,0 +1,30 @@
+package ch06_Sorting.exam02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class exam02_2 {
+	
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		Integer[] arr = new Integer[n];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine()); 
+		}
+		
+		Arrays.sort(arr, Collections.reverseOrder());
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+}
